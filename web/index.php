28e05e1af973f32ea6296469d9910b53ef443d43
Tim Falken <tfalken@kvt.nl>
2026-01-15 12:35:19 +0100

    favicon

--- web/index.php
+++ web/index.php
@@ -178,6 +178,12 @@ usort($projects, fn($a, $b) => strcmp((string) $a['No'], (string) $b['No']));
       }
     }
   </style>
+
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+  <link rel="manifest" href="site.webmanifest">
+  </body>
 </head>
 
 <body>
